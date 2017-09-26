b16493c421fb3d0cfba703b1d3e6f6bc2493ebbb
594035 <michaelortiz@C02V40NSHTDG.local>
2017-09-26 13:35:03 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,31 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [];
+
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0]===cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+	}
+}
+
+var checkForMatch = function () {
+if (cardsInPlay[0]===cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+	}
+}
+
+
+var flipCard = function(cardId){
+console.log("User flipped" + " " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
