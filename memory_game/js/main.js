12d996080a0c95b3d0eb8228e7c6052cad8a488e
594035 <michaelortiz@c02v40nshtdg.users.bah.com>
2017-10-03 10:33:43 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -34,6 +34,7 @@ if (cardsInPlay.length === 2) {
 }
 
 var checkForMatch = function () {
+this.setAttribute("src", cards[cardId].cardImage)
 if (cardsInPlay[0]===cardsInPlay[1]){
 		alert("You found a match!");
 	} else {
@@ -42,7 +43,8 @@ if (cardsInPlay[0]===cardsInPlay[1]){
 }
 
 
-var flipCard = function(cardId){
+var flipCard = function(){
+var cardId = this.getAttribute('data-id');
 console.log("User flipped" + " " + cards[cardId].rank);
 console.log(cards[cardId].cardImage)
 console.log(cards[cardId].rank)
@@ -50,6 +52,14 @@ cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for (var i =0; i<cards.length;i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById("game-board").appendChild(cardElement)
+	};
+};
 
+createBoard();
